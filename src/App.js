813176ce2d5e5db1a34ed1bcde988e4880ea1140
WAQAS <muhammadwaqas0349@gmail.com>
2024-04-12 12:07:01 +0200

    first commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import Counter from "./features/counter/Counter";
+
+function App() {
+  
+  return (
+    <main className="App">
+
+      <Counter />
+
+    </main>
+  );
+}
+
+export default App;
